0fd663ddc65d6604b7fc296c82a3b3a2806c9f9b
Bhoomil Dayani <bhoomildayani182@gmail.com>
2022-08-16 22:05:53 +0530

    Getdata studentlist

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
 import Navigationbar from './Component/Navigationbar';
 import Addstudent from './Component/Addstudent';
-//import getdata from './Component/getdata';
+import Getdata from './Component/Getdata';
 import { Container } from 'react-bootstrap';
 import ReactDOM from 'react-dom/client';
 import Footer from "./Component/Footer";
+
 
 
 import {
@@ -22,7 +23,9 @@ function App() {
         <Routes>
           <Route path='/adddata' element={<Addstudent />} />
         </Routes>
-
+        <Routes>
+          <Route path='/getdata' element={<Getdata />} />
+        </Routes>
 
       </BrowserRouter>
       <Footer />
